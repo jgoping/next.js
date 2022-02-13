d7f492841bba20baed5ae2aeeee53558b5926f43
Justin Goping <jgoping@google.com>
2022-02-13 20:15:34 +0000

    Allow policy to be null when Trusted Types are not supported

--- packages/next/client/trusted-types.ts
+++ packages/next/client/trusted-types.ts
@@ -1,12 +1,13 @@
-let policy: TrustedTypePolicy | undefined
+let policy: TrustedTypePolicy | null | undefined
 
 function getPolicy() {
-  if (!policy && typeof window !== 'undefined') {
-    policy = window.trustedTypes?.createPolicy('nextjs', {
-      createHTML: (input) => input,
-      createScript: (input) => input,
-      createScriptURL: (input) => input,
-    })
+  if (typeof policy === 'undefined' && typeof window !== 'undefined') {
+    policy =
+      window.trustedTypes?.createPolicy('nextjs', {
+        createHTML: (input) => input,
+        createScript: (input) => input,
+        createScriptURL: (input) => input,
+      }) || null
   }
 
   return policy
@@ -15,5 +16,5 @@ function getPolicy() {
 export function __unsafeCreateTrustedScriptURL(
   url: string
 ): TrustedScriptURL | string {
-  return getPolicy()?.createScriptURL(url) ?? url
+  return getPolicy()?.createScriptURL(url) || url
 }
