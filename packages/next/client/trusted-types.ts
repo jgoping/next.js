45f5500b63660c480c918e84661ba45b96d81e70
Justin Goping <jgoping@google.com>
2022-02-15 21:55:21 +0000

    Add documentation to trusted-types.ts

--- packages/next/client/trusted-types.ts
+++ packages/next/client/trusted-types.ts
@@ -1,5 +1,13 @@
+/**
+ * Stores the Trusted Types Policy. Starts as undefined and can be set to null
+ * if Trusted Types is not supported in the browser.
+ */
 let policy: TrustedTypePolicy | null | undefined
 
+/**
+ * Getter for the Trusted Types Policy. If it is undefined, it is instantiated
+ * here or set to null if Trusted Types is not supported in the browser.
+ */
 function getPolicy() {
   if (typeof policy === 'undefined' && typeof window !== 'undefined') {
     policy =
@@ -13,6 +21,15 @@ function getPolicy() {
   return policy
 }
 
+/**
+ * Unsafely promote a string to a TrustedScriptURL, falling back to strings
+ * when Trusted Types are not available.
+ * This is a security-sensitive function; any use of this function
+ * must go through security review. In particular, it must be assured that the
+ * provided string will never cause an XSS vulnerability if used in a context
+ * that will cause a browser to load and execute a resource, e.g. when
+ * assigning to script.src.
+ */
 export function __unsafeCreateTrustedScriptURL(
   url: string
 ): TrustedScriptURL | string {
